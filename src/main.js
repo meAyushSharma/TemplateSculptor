3ed86f95e541bc45a3963127774f331c81dbd631
meAyushSharma <iamayushsharma60@gmail.com>
2024-03-23 10:40:13 +0530

    Completed main site

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
+var logo = document.getElementById("logo-id");
+logo.addEventListener("click", function () {
+  navigateToSection("top-here");
+});
 
+var about = document.getElementsByClassName("about-container")[0];
+about.addEventListener("click", function () {
+  navigateToSection("about");
+});
+var template = document.getElementsByClassName("templates-container")[0];
+template.addEventListener("click", function () {
+  navigateToSection("template");
+});
+var navigate = document.getElementsByClassName("material-symbols-outlined")[0];
+navigate.addEventListener("click", function () {
+  navigateToSection("top-here");
+});
+
+function navigateToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: "smooth" });
+  history.pushState(null, null, "#" + sectionId);
+}
